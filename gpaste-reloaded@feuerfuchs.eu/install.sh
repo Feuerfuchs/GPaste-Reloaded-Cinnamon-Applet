740456a538b6d8399e48ef9fc1acbac4944721c0
Feuerfuchs <vulpes.ignitus+github@gmail.com>
2016-08-15 01:48:25 +0200

    Made some improvements to the install script

--- gpaste-reloaded@feuerfuchs.eu/install.sh
+++ gpaste-reloaded@feuerfuchs.eu/install.sh
@@ -6,42 +6,45 @@ if [ $# -eq 0 ]
 then
     echo "[*] Installing localizations..."
 
-    cinnamon-json-makepot -i ./po/* &> /dev/null
+    cinnamon-json-makepot -i ./po/* > /dev/null
 
     echo "[*] Installing applet files..."
+
     [ -d "$APPLET_DIR" ] || mkdir "$APPLET_DIR"
-    cp ./* "$APPLET_DIR" &> /dev/null
+    find . -maxdepth 1 -type f ! -name $(basename -- "$0") -exec cp {} "$APPLET_DIR" \;
 
     echo "[*] Done!"
-else
-    while getopts "rh" OPTION; do
-        case $OPTION in
-            r)
-                echo "[*] Removing localizations..."
-
-                cinnamon-json-makepot -r ./po/* &> /dev/null
-
-                echo "[*] Removing applet files..."
-
-                rm -rf "$APPLET_DIR" &> /dev/null
-
-                echo "[*] Done!"
-                ;;
-
-            h)
-                echo "Usage:"
-                echo ""
-                echo "      install.sh [-r | -h]"
-                echo ""
-                echo "      If no arguments are supplied, the applied gets installed."
-                echo "      If -r is supplied, the applet gets removed instead."
-                echo "      If -h is supplied, this help message is displayed."
-                echo ""
-                echo "Options:"
-                echo "  -r  remove the applet"
-                echo "  -h  help (this output)"
-                exit 0
-                ;;
-        esac
-    done
+    exit 0
 fi
+
+while getopts "rh" OPTION; do
+    case $OPTION in
+        r)
+            echo "[*] Removing localizations..."
+
+            cinnamon-json-makepot -r ./po/* > /dev/null
+
+            echo "[*] Removing applet files..."
+
+            rm -rf "$APPLET_DIR" > /dev/null
+
+            echo "[*] Done!"
+            exit 0
+            ;;
+
+        h)
+            echo "Usage:"
+            echo ""
+            echo "      install.sh [-r | -h]"
+            echo ""
+            echo "      If no arguments are supplied, the applied gets installed."
+            echo "      If -r is supplied, the applet gets removed instead."
+            echo "      If -h is supplied, this help message is displayed."
+            echo ""
+            echo "Options:"
+            echo "  -r  remove the applet"
+            echo "  -h  help (this output)"
+            exit 0
+            ;;
+    esac
+done
